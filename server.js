2d02d250c059a109a27f4c7137c03aa1bbf135e9
zarinzaidi <aereinzayed@yahoo.com>
2024-04-27 16:33:55 +0800

    Clean up

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
+//Start the server
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
